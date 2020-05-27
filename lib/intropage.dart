7eb81a419a03b62bbebb5c27dc67f0e9048543d7
Parthav <parthavjoshi1718@gmail.com>
2020-05-27 23:52:29 +0530

    Updated DevInfo

--- lib/intropage.dart
+++ lib/intropage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
 class IntroPage extends StatelessWidget {
   @override
@@ -10,8 +11,62 @@ class IntroPage extends StatelessWidget {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          SizedBox(
-            height: 80,
+          Container(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Stack(
+                    alignment: Alignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 100.0,
+                        height: 100.0,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xFFF0EFF4)),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                            image: AssetImage('assets/images/Devlogo.jpeg'),
+                          ),
+                        ),
+                      ),
+                    ],
+                    overflow: Overflow.clip,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Developed by',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Parthav Joshi',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(EvaIcons.github),
+                      ),
+                      IconButton(
+                        icon: Icon(EvaIcons.twitter),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
             width: MediaQuery.of(context).size.width * 0.9,
@@ -20,36 +75,6 @@ class IntroPage extends StatelessWidget {
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15.0)),
               margin: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(children: <Widget>[
-                    SizedBox(
-                      height: 6,
-                    ),
-                    CircleAvatar(
-                      child: Padding(
-                        padding: const EdgeInsets.all(11.0),
-                        child: Image.asset(
-                          'assets/images/Devlogo.jpeg',
-                          width: 100,
-                          height: 100,
-                        ),
-                      ),
-                      radius: 40,
-                      backgroundColor: Colors.white,
-                    ),
-                    SizedBox(
-                      height: 6,
-                    ),
-                  ]),
-                  Text(
-                    "</> by️\nParthav Joshi",
-                    style:
-                    Theme.of(context).appBarTheme.textTheme.title,
-                  )
-                ],
-              ),
             ),
           ),
           SizedBox(
@@ -78,8 +103,7 @@ class IntroPage extends StatelessWidget {
                 Container(
                   child: Text(
                     "Bugs Found!! \nFeature Suggestions??\nCreate a new issue on GitHub to let me know, or contibute by forking and sending a Personal Response",
-                    style:
-                    TextStyle(fontFamily: 'OpenSans', fontSize: 12),
+                    style: TextStyle(fontFamily: 'OpenSans', fontSize: 12),
                   ),
                   padding: EdgeInsets.all(10),
                   margin: EdgeInsets.all(10),
